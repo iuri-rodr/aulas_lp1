4577925b02a63becb0369494f48576f094ede88f
iuri <iurirodrigues2002@gmail.com>
2020-10-30 19:38:32 +0000

    all

---
+++ teste programaçao/test.c
@@ -0,0 +1,36 @@
+/**
+ * @file test.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ 
+
+ /**
+ * @file i
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+
+*/
+
+
+
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x = 1;
+    printf("%d\n", x);
+    return 0;
+}
